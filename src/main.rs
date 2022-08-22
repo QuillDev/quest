523013f4ff8632e7dcee88865dce45fb84cbbe1c
Robert Brunson <robert.brunson@ext.superleague.com>
2022-08-22 00:14:30 -0400

    changed default behavior with no subarg to show status

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ mod data;
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(subcommand)]
-    command: Commands,
+    command: Option<Command>,
 }
 
 #[derive(Subcommand)]
-enum Commands {
+enum Command {
     /// Shows Quests that are currently available
     Show,
     /// Completed the quest with the given name
@@ -39,18 +39,21 @@ fn main() {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::Show => {
+        Some(Command::Show) => {
             profile.print_status();
         }
-        Commands::Add { name, exp, daily } => {
+        Some(Command::Add { name, exp, daily }) => {
             let quest = Quest::new(name, exp.clone(), daily.is_some());
             profile.add_quest(quest);
         }
-        Commands::Remove {name} => {
+        Some(Command::Remove {name}) => {
             profile.remove_quest(name);
         }
-        Commands::Complete { name } => {
+        Some(Command::Complete { name }) => {
             profile.complete(name);
+        }
+        None => {
+            profile.print_status();
         }
     }
 }
